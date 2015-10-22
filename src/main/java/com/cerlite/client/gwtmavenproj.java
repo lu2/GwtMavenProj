01075fb083647af4f819cfe81eec01adde1e4814
Ludvik Haltuf <ludvik.haltuf@gmail.com>
2015-10-23 01:17:44 +0200

    Direct testarea check, this is ok

--- src/main/java/com/cerlite/client/gwtmavenproj.java
+++ src/main/java/com/cerlite/client/gwtmavenproj.java
@@ -3,6 +3,7 @@ package com.cerlite.client;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.vaadin.polymer.paper.widget.PaperButton;
+import com.vaadin.polymer.paper.widget.PaperTextarea;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -13,9 +14,16 @@ public class gwtmavenproj implements EntryPoint {
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		// Use Widget API to Create a <paper-button>
 		PaperButton button = new PaperButton("Press me!");
+		PaperTextarea textarea = new PaperTextarea();
+		PaperTextarea textarea2 = new PaperTextarea();
 		button.setRaised(true);
 		RootPanel.get().add(button);
+		RootPanel.get().add(textarea);
+		RootPanel.get().add(textarea2);
+		textarea.setLabel("testLabel");
+		textarea.setValue("test");
+		textarea2.setLabel("testLabel2");
+		textarea2.setValue("test2");
 	}
 }
