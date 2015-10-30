a4eae269b7e5e8b187fe57b1aedadb7e424c96f2
Ludvik Haltuf <ludvik.haltuf@gmail.com>
2015-10-30 21:21:35 +0100

    Leave only code necessary for spotting the issue.

--- src/main/java/com/cerlite/client/gwtmavenproj.java
+++ src/main/java/com/cerlite/client/gwtmavenproj.java
@@ -2,23 +2,12 @@ package com.cerlite.client;
 
 import java.util.Arrays;
 
-import com.cerlite.client.view.Content;
 import com.cerlite.client.view.Main;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.vaadin.polymer.Polymer;
 import com.vaadin.polymer.elemental.Function;
-import com.vaadin.polymer.iron.element.IronIconElement;
-import com.vaadin.polymer.paper.element.PaperButtonElement;
-import com.vaadin.polymer.paper.element.PaperDialogElement;
-import com.vaadin.polymer.paper.element.PaperDrawerPanelElement;
-import com.vaadin.polymer.paper.element.PaperHeaderPanelElement;
-import com.vaadin.polymer.paper.element.PaperIconButtonElement;
-import com.vaadin.polymer.paper.element.PaperIconItemElement;
-import com.vaadin.polymer.paper.element.PaperMaterialElement;
-import com.vaadin.polymer.paper.element.PaperRippleElement;
 import com.vaadin.polymer.paper.element.PaperTextareaElement;
-import com.vaadin.polymer.paper.element.PaperToolbarElement;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -26,8 +15,6 @@ import com.vaadin.polymer.paper.element.PaperToolbarElement;
 public class gwtmavenproj implements EntryPoint {
 	
 	private Main mainView;
-	private Content contentView;
-	private Content contentView2;
 
 	/**
 	 * This is the entry point method.
@@ -35,9 +22,7 @@ public class gwtmavenproj implements EntryPoint {
 	public void onModuleLoad() {
 		// We have to load icon sets before run application
 				Polymer.importHref(
-						Arrays.asList(
-								PaperTextareaElement.SRC,
-								PaperButtonElement.SRC), 
+						Arrays.asList(PaperTextareaElement.SRC), 
 						new Function() {
 							public Object call(Object arg) {
 								// The app is executed when all imports succeed.
@@ -49,21 +34,8 @@ public class gwtmavenproj implements EntryPoint {
 	}
 
 	protected void startApplication() {
-		mainView = new Main(this);
+		mainView = new Main();
 		RootPanel.get().add(mainView.getWidget());
-		
-		contentView = new Content();
-		contentView2 = new Content();
-		
-		mainView.getChildContent().add(contentView.getWidget());
-		mainView.getChildContent().add(contentView2.getWidget());
-		
 	}
 
-	public void clicked() {
-		contentView.setLabel2("parentClickLbl");
-		contentView.setValue2("parentClickVal");
-		contentView2.setLabel2("parentClickLbl");
-		contentView2.setValue2("parentClickVal");
-	}
 }
--- src/main/java/com/cerlite/client/view/Main.java
+++ src/main/java/com/cerlite/client/view/Main.java
@@ -1,18 +1,10 @@
 package com.cerlite.client.view;
 
-import com.cerlite.client.gwtmavenproj;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.client.ui.HasWidgets;
 import com.google.gwt.user.client.ui.Widget;
-import com.vaadin.polymer.elemental.Event;
-import com.vaadin.polymer.elemental.EventListener;
-import com.vaadin.polymer.paper.element.PaperButtonElement;
 import com.vaadin.polymer.paper.element.PaperTextareaElement;
-import com.vaadin.polymer.paper.widget.PaperButton;
 import com.vaadin.polymer.paper.widget.PaperTextarea;
 
 public class Main {
@@ -23,42 +15,25 @@ public class Main {
 	}
 	
 	private final Widget widget;
-	private final gwtmavenproj parent;
-	
-	@UiField
-	HasWidgets content;
 	
 	@UiField 
-	PaperTextareaElement textarea1;
+	PaperTextarea textareaWidget;
 	@UiField 
-	PaperTextareaElement textarea2;
-	
-	@UiField
-	PaperButtonElement button1;
+	PaperTextareaElement textareaElement;
 
-	public Main(gwtmavenproj gwtmavenproj) {
+	public Main() {
 		widget = uiBinder.createAndBindUi(this);
-		parent = gwtmavenproj;
-		textarea1.setValue("testvalue");
-		textarea1.setLabel("testlabel");
-		textarea2.setValue("testvalue2");
-		textarea2.setLabel("testlabel2");
 		
-		button1.addEventListener("click", new EventListener() {
-			
-			@Override
-			public void handleEvent(Event event) {
-				parent.clicked();
-			}
-		});
+		textareaWidget.setValue("testvalue");
+		textareaWidget.setLabel("testlabel");
+		textareaElement.setValue("testvalue2");
+		textareaElement.setLabel("testlabel2");
+	
 	}
 	
 	public Widget getWidget() {
 		return widget;
 	}
-	
-	public HasWidgets getChildContent() {
-		return content;
-	}
+
 
 }
